4383b7bea1dd674aa687348ed6b8aa6ca6338533
moe <moe@kandi-care.co.uk>
2023-01-30 14:59:36 +0000

    test

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -6,7 +6,7 @@ module "ec2_instance" {
 
   ami                    = data.aws_ami.latest-amazon-image.id
   instance_type          = var.instance_type
-  key_name               = aws_key_pair.ssh-key.key_name
+  #key_name               = aws_key_pair.ssh-key.key_name
   monitoring             = false
   vpc_security_group_ids = [var.vpc_security_group_ids]
   subnet_id              = var.subnet_id
@@ -15,11 +15,12 @@ module "ec2_instance" {
  
 }
 
-resource "aws_key_pair" "ssh-key" {
-    key_name = "server_key"
-    public_key = file(var.my_public_key_location)
-  
-}
+# resource "aws_key_pair" "ssh-key" {
+#     key_name = "server_key"
+#     public_key = "C://Users//moe//.ssh//id_rsa"
+
+
+# }
 
 data "aws_ami" "latest-amazon-image" {
     most_recent = true 
